dd20093795d79da4c37f1d062187c1fb67c1a16e
Rakesh Jadhav <thisisganesh353@gmail.com>
2025-07-02 18:38:17 +0530

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.9.2" apply false
+    id("com.android.application") version "8.11.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
@@ -12,7 +12,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.9.2")
+        classpath("com.android.tools.build:gradle:8.11.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
     }
