856eecea1de72997425d5d18b8c7db918fdbe36c
Rakesh Jadhav <thisisganesh353@gmail.com>
2025-07-02 19:16:12 +0530

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application") version "8.11.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
 }
 
@@ -14,12 +14,12 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.11.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.56.2")
     }
 }
 
 
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(layout.buildDirectory)
 }
